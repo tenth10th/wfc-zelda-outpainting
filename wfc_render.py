a78f68696278f69d0e7a91b001205f9ddd03a124
Glyph <code@glyph.im>
2024-01-17 17:21:49 -0800

    add scrolling based on fritter

--- wfc_render.py
+++ wfc_render.py
@@ -1,9 +1,11 @@
 import itertools
+
 import pygame
+from fritter.boundaries import Cancellable
 from pygame import Surface
 from pygame.font import Font
 
-
+# fmt: off
 TILE_SOLIDITY = [
     0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0,
     0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0,
@@ -14,6 +16,7 @@ TILE_SOLIDITY = [
     1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
 ]
+# fmt: on
 
 
 def load_font() -> Font:
@@ -34,12 +37,13 @@ def load_tile_images() -> list[Surface]:
 
 def load_image(path: str, size: int = 30, remove_padding: int = 0) -> Surface:
     surface = pygame.image.load(path).convert_alpha()
-    surface = pygame.transform.scale(surface, (size - remove_padding, size - remove_padding))
+    surface = pygame.transform.scale(
+        surface, (size - remove_padding, size - remove_padding)
+    )
     return surface
 
 
 def parse_tile_map() -> list[list[int]]:
-
     int_map = []
 
     # Also from the Zelda Walking Tour: https://github.com/asweigart/nes_zelda_map_data
@@ -74,11 +78,16 @@ def render_tileset(display: Surface, tiles: list[Surface], font: Font):
             y += 30
             x = 0
 
-    render_message(display, 'Walking Tour tileset, re-rendered \"in engine\"', 5, 250, font)
+    render_message(
+        display, 'Walking Tour tileset, re-rendered "in engine"', 5, 250, font
+    )
 
-    render_message(display, '(space to continue)', 5, 300, font)
+    render_message(display, "(space to continue)", 5, 300, font)
 
-def render_tiles_by_solidity(display: Surface, tiles: list[Surface], TILE_SOLIDITY: list[int], font: Font):
+
+def render_tiles_by_solidity(
+    display: Surface, tiles: list[Surface], TILE_SOLIDITY: list[int], font: Font
+):
     # Render tiles to pygame surface
     # (simulating zelda_overworld_tiles.png)
     x = 0
@@ -110,7 +119,15 @@ def render_tiles_by_solidity(display: Surface, tiles: list[Surface], TILE_SOLIDI
 
     render_message(display, "(space to continue)", 5, 380, font)
 
-def render_map_quadrant(tiles: list[Surface], map_data: list[list[int]], from_x: int, from_y: int, display: Surface, font: Font):
+
+def render_map_quadrant(
+    tiles: list[Surface],
+    map_data: list[list[int]],
+    from_x: int,
+    from_y: int,
+    display: Surface,
+    font: Font,
+):
     for y_offset, x_offset in itertools.product(range(20), range(20)):
         tile_id = map_data[from_y + y_offset][from_x + x_offset]
         tile = tiles[tile_id]
@@ -123,9 +140,23 @@ def render_map_quadrant(tiles: list[Surface], map_data: list[list[int]], from_x:
     render_message(display, "(space to continue)", 5, 35, font)
 
 
-def render_message(display: Surface, text: str, x: int, y: int, font: Font, r: int = 255, g: int = 255, b: int = 255) -> None:
+def render_message(
+    display: Surface,
+    text: str,
+    x: int,
+    y: int,
+    font: Font,
+    r: int = 255,
+    g: int = 255,
+    b: int = 255,
+) -> None:
     message_surface = font.render(text, 1, (r, g, b))
     display.blit(message_surface, (x, y))
+
+
+from fritter.drivers.sleep import SleepDriver
+from fritter.repeat import EverySecond, repeatedly
+from fritter.scheduler import SimpleScheduler
 
 
 def main() -> None:
@@ -136,10 +167,42 @@ def main() -> None:
     loop = True
     x = 0
     y = 0
-    max_y = len(overworld_map)-20
-    max_x = len(overworld_map[0])-20
+    max_y = len(overworld_map) - 20
+    max_x = len(overworld_map[0]) - 20
+    keys = set()
 
     mode = 1
+
+    scheduler = SimpleScheduler(driver := SleepDriver())
+
+    def go_up() -> None:
+        nonlocal y
+        y = max(y - 1, 0)
+
+    def go_down() -> None:
+        nonlocal y
+        y = min(y + 1, max_y)
+
+    def go_left() -> None:
+        nonlocal x
+        x = max(x - 1, 0)
+
+    def go_right() -> None:
+        nonlocal x
+        x = min(x + 1, max_x)
+
+    def do_moves(steps: int, stopper: Cancellable) -> None:
+        for pressed_key in keys:
+            movement[pressed_key]()
+
+    movement = {
+        pygame.K_UP: go_up,
+        pygame.K_DOWN: go_down,
+        pygame.K_LEFT: go_left,
+        pygame.K_RIGHT: go_right,
+    }
+
+    repeatedly(scheduler, do_moves, EverySecond(1 / 60))
 
     while loop:
         display.fill((0, 0, 0))
@@ -152,33 +215,26 @@ def main() -> None:
             render_map_quadrant(tiles, overworld_map, x, y, display, font)
 
         for event in pygame.event.get():
-
             if event.type == pygame.KEYDOWN:
-
                 if event.key == pygame.K_SPACE:
                     mode = 1 if mode == 3 else mode + 1
-
-                if event.key == pygame.K_UP:
-                    y = max(y-1, 0)
-                if event.key == pygame.K_DOWN:
-                    y = min(y+1, max_y)
-                if event.key == pygame.K_LEFT:
-                    x = max(x-1, 0)
-                if event.key == pygame.K_RIGHT:
-                    x = min(x+1, max_x)
-
+                if event.key in movement:
+                    keys.add(event.key)
                 if event.key in (pygame.K_q, pygame.K_ESCAPE):
                     loop = False
 
+            if event.type == pygame.KEYUP:
+                if event.key in movement:
+                    keys.remove(event.key)
+
             if event.type == pygame.QUIT:
                 loop = False
-
+        driver.block(0)
         pygame.display.flip()
-
 
     print("Exiting...")
     exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
